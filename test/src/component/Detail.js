50dfbaa55f2dc4c865ea68d18a6906a8ea7cb4f0
hikarut <ht.hikaru.takahashi@gmail.com>
2018-01-14 10:40:46 +0900

    add style in webview

--- test/src/component/Detail.js
+++ test/src/component/Detail.js
@@ -3,24 +3,43 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { WebView } from 'react-native'
+import { WebView, StyleSheet } from 'react-native'
 
 console.log('Detail.js')
+
+// 型の定義
+type State = {
+  loaded: boolean
+}
 
 // 型の定義
 type Props = {
   navigation: Object
 }
 
-export default class Detail extends Component<Props> {
+export default class Detail extends Component<Props, State> {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.title
   })
+
+  constructor() {
+    super()
+    // 初期化
+    this.state = {
+      loaded: false
+    }
+  }
 
   render() {
     const { params } = this.props.navigation.state
     console.log(params)
 
-    return <WebView source={{ uri: params.url }} />
+    return <WebView source={{ uri: params.url }} style={styles.view} />
   }
 }
+
+const styles = StyleSheet.create({
+  view: {
+    marginBottom: 40
+  }
+})
