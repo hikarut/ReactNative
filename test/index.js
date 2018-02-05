23f6f7e5647c36b1746c861c8635ed011094ac59
hikarut <ht.hikaru.takahashi@gmail.com>
2018-02-05 22:04:18 +0900

    refactoring and add store

--- test/index.js
+++ test/index.js
@@ -1,7 +1,5 @@
 /* @flow */
 import { AppRegistry } from 'react-native'
-// import App from './src/App'
 import Main from './src/components/Main'
 
-// AppRegistry.registerComponent('test', () => App)
 AppRegistry.registerComponent('test', () => Main)
