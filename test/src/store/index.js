3deee77d99abfab1a6fb5db4b56fd1eb63c7fcfb
hikarut <ht.hikaru.takahashi@gmail.com>
2018-03-21 16:06:47 +0900

    add store

--- test/src/store/index.js
+++ test/src/store/index.js
@@ -1,3 +1,12 @@
 /* @flow */
 
 'use strict'
+
+import { createStore } from 'redux'
+import rootReducer from '../reducers'
+
+const store = (initialState: Object) => {
+  return createStore(rootReducer, initialState)
+}
+
+export default store
