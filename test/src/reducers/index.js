52e507bc6496d1c3a3fe887e5beb7aeab6901f11
hikarut <ht.hikaru.takahashi@gmail.com>
2018-03-21 15:56:01 +0900

    add reducers

--- test/src/reducers/index.js
+++ test/src/reducers/index.js
@@ -1,3 +1,30 @@
 /* @flow */
 
 'use strict'
+
+export const getThreads = (state: boolean = false, action: Object) => {
+  switch (action.type) {
+    case 'GET_THREADS':
+      return action.threads
+    default:
+      return state
+  }
+}
+
+export const loadData = (state: boolean = false, action: Object) => {
+  switch (action.type) {
+    case 'LOADING':
+      return action.loaded
+    default:
+      return state
+  }
+}
+
+export const getError = (state: boolean = false, action: Object) => {
+  switch (action.type) {
+    case 'ERROR':
+      return action.hasError
+    default:
+      return state
+  }
+}
