4e5aee755b288c3ed31568a1dc946f0dc89dc73e
hikarut <ht.hikaru.takahashi@gmail.com>
2018-05-11 08:21:45 +0900

    rm file

--- test/index.js
+++ test/index.js
@@ -2,9 +2,7 @@
 
 import React from 'react'
 import { AppRegistry } from 'react-native'
-// import Main from './src/components/Main'
-import Main from './src/components/MainV2'
-// import ReduxSample from './src/components/ReduxSample'
+import Main from './src/components/Main'
 import { Provider } from 'react-redux'
 import configureStore from './src/store'
 
