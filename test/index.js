ae19b5bf4675db9c0d95bfbb0f0ada2a61235891
hikarut <ht.hikaru.takahashi@gmail.com>
2018-05-17 23:19:31 +0900

    warnig ignore

--- test/index.js
+++ test/index.js
@@ -5,6 +5,13 @@ import { AppRegistry } from 'react-native'
 import Main from './src/components/Main'
 import { Provider } from 'react-redux'
 import configureStore from './src/store'
+import { YellowBox } from 'react-native'
+
+// Warningの無視
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Module RCTImageLoader'
+])
 
 const store = configureStore()
 
